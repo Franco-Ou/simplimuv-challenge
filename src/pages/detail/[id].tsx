74faefc673c68b83e44a980bf3b78e3a80cf074a
Franco-Ou <franco.oubel@gmail.com>
2023-11-02 17:43:33 -0300

    feat: bringing data by id

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -1,4 +1,5 @@
 import DetailContainer from "@components/DetailContainer";
+import { ObjectId } from "mongodb";
 import { GetServerSideProps } from "next";
 import React, { FC } from "react";
 import { Product } from "src/interfaces/products";
@@ -12,11 +13,21 @@ const ProductDetailPage: FC<{ product: Product }> = ({ product }) => {
   );
 };
 
-/* export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { id } = params as { id: string };
+
   const db = await connectToDatabase();
   const collection = await db.collection("bikes");
 
-  const product = await collection.findById(query.id).toArray();
+  const product = await collection.findOne({
+    _id: new ObjectId(id),
+  });
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
 
   const serializableData = {
     ...product,
@@ -26,37 +37,6 @@ const ProductDetailPage: FC<{ product: Product }> = ({ product }) => {
   return {
     props: { product: serializableData },
   };
-}; */
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  try {
-    const { id } = params as { id: string };
-
-    const db = await connectToDatabase();
-    const collection = db.collection("bikes");
-
-    const product = await collection.findOne({ _id: id });
-
-    if (!product) {
-      return {
-        notFound: true,
-      };
-    }
-
-    const serializableData = {
-      ...product,
-      _id: product._id.toString(),
-    };
-
-    return {
-      props: { product: serializableData },
-    };
-  } catch (error) {
-    console.error("Error fetching data from MongoDB:", error);
-    return {
-      notFound: true, // Handle the error by marking the page as not found
-    };
-  }
 };
 
 export default ProductDetailPage;
